856d6d0893c650d748b3915cd74d78b770b2b722
Jeremy Bloomstrom <jeremy@in.genio.us>
2018-08-06 16:24:15 -0800

    Add Header info

--- resources/themes/default/views/header.blade.php
+++ resources/themes/default/views/header.blade.php
@@ -1,1 +1,13 @@
-<header></header>
+
+<center style="margin-top:2em">
+<header>
+    <h1>MSB July 2018 CyberAttack</h1>
+    <h2>File Sharing Platform</h2>
+</header>
+</center>
+
+<p style="background: lime; margin: 1em auto; width: 770px; padding: 2em; font-size: 20px; border-radius: 20px;">
+    <strong>Important</strong> This site is for sharing "green" files only. If your data has not been cleared for use on the "green" network, do not share it.
+</p>
+
+
